99fdb237412027e1f6ddb8fb7b02f811132346a6
strangecousinwst <139879737+strangecousinwst@users.noreply.github.com>
2025-04-14 23:45:07 +0100

    1st commit

---
+++ src/main/java/com/negocios/denuncias/DenunciasApplication.java
@@ -0,0 +1,13 @@
+package com.negocios.denuncias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenunciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenunciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/negocios/denuncias/DenunciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.negocios.denuncias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenunciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
