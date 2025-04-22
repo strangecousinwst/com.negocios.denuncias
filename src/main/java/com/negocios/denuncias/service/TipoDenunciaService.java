f4d31cbd2039f693fdf47dbcb00a8a1e866ce5de
Diogo Castilho <151971935+castdi5@users.noreply.github.com>
2025-04-22 13:18:42 +0100

    tipoDenuncia finalizado + TODO LIST

--- src/main/java/com/negocios/denuncias/service/TipoDenunciaService.java
+++ src/main/java/com/negocios/denuncias/service/TipoDenunciaService.java
@@ -1,41 +1,94 @@
 package com.negocios.denuncias.service;
 
+import com.negocios.denuncias.database.DbMySQL;
+import com.negocios.denuncias.model.Denuncia;
 import com.negocios.denuncias.model.TipoDenuncia;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 // @Component
 @Service
 public class TipoDenunciaService {
 
-    private Map<String, TipoDenuncia> td = new HashMap<>() {{
-        put("1", new TipoDenuncia( "Utilizador"));
-        put("2", new TipoDenuncia("Comentário"));
-        put("3", new TipoDenuncia("Publicação"));
-    }};
 
-    public Collection<TipoDenuncia> get() {
-        return td.values();
+    private DbMySQL dbMySQL;
+    private ArrayList<TipoDenuncia> denuncias;
+    private Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    public TipoDenunciaService(DbMySQL dbMySQL) throws SQLException {
+        this.dbMySQL = dbMySQL;
+        conn = dbMySQL.getConnection();
     }
 
-    public TipoDenuncia get(String id) {
-        return td.get(id);
+//    private Map<String, TipoDenuncia> td = new HashMap<>() {{
+//        put("1", new TipoDenuncia( "Utilizador"));
+//        put("2", new TipoDenuncia("Comentário"));
+//        put("3", new TipoDenuncia("Publicação"));
+//    }};
+
+    public ArrayList<TipoDenuncia> get() throws SQLException {
+        ArrayList<TipoDenuncia> tipodenuncias = new ArrayList<>();
+        String query = "SELECT * FROM tipo_denuncia";
+        ps = conn.prepareStatement(query);
+        rs = ps.executeQuery();
+        while (rs.next()) {
+            TipoDenuncia td = new TipoDenuncia();
+            td.setId(rs.getInt("id"));
+            td.setDescricao(rs.getString("descricao"));
+            tipodenuncias.add(td);
+        }
+        return tipodenuncias;
     }
 
-    public TipoDenuncia remove(String id) {
-        return td.remove(id);
+    public TipoDenuncia get(int id) throws SQLException {
+        TipoDenuncia td = new TipoDenuncia();
+        String query = "SELECT * FROM tipo_denuncia WHERE id = ?";
+        ps = conn.prepareStatement(query);
+        ps.setInt(1, id);
+        rs = ps.executeQuery();
+        while (rs.next()) {
+            td.setId(rs.getInt("id"));
+            td.setDescricao(rs.getString("descricao"));
+        }
+        return td ;
     }
 
-    public TipoDenuncia save(@Valid String descricao) {
-        TipoDenuncia novoTipo = new TipoDenuncia();
-        novoTipo.setId(UUID.randomUUID().toString());
-        novoTipo.setDescricao(descricao);
-        td.put(novoTipo.getId(), novoTipo);
-        return novoTipo;
+    public TipoDenuncia remove(int id) throws SQLException {
+        TipoDenuncia td = new TipoDenuncia();
+        String query = "DELETE FROM tipo_denuncia WHERE id = ?";
+        ps = conn.prepareStatement(query);
+        ps.setInt(1, id);
+        ps.executeUpdate();
+        return td;
     }
+
+    public void save(TipoDenuncia td) throws SQLException {
+        String query = "INSERT INTO tipo_denuncia (descricao) VALUES (?)";
+        ps = conn.prepareStatement(query);
+        ps.setString(1, td.getDescricao());
+        ps.executeUpdate();
+    }
+
+    public void update(TipoDenuncia td) throws SQLException {
+        String query = "UPDATE tipo_denuncia SET descricao = ? WHERE id = ?";
+        ps = conn.prepareStatement(query);
+        ps.setString(1, td.getDescricao());
+        ps.setInt(2, td.getId());
+        ps.executeUpdate();
+    }
+//    public TipoDenuncia save(@Valid String descricao) {
+//        TipoDenuncia novoTipo = new TipoDenuncia();
+//        novoTipo.setId(UUID.randomUUID().toString());
+//        novoTipo.setDescricao(descricao);
+//        td.put(novoTipo.getId(), novoTipo);
+//        return novoTipo;
+//    }
 }
